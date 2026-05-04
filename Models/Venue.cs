99eff93c59c289746d1e32b66c56ac1a433d49d8
lukeFourieST10443474 <166035660+lukfou@users.noreply.github.com>
2026-05-04 11:41:08 +0200

    Add files via upload

---
+++ Models/Venue.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace CLDV6211POE.Models
+{
+    public class Venue
+    {
+        public int VenueId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public int Capacity { get; set; }
+
+        // ✅ ADD THIS
+        public string? Description { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+    }
+}
