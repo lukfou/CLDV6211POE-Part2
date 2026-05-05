018cc215e7bb42ca9f84f28d0cf12bff551ac27f
lukeFourieST10443474 <166035660+lukfou@users.noreply.github.com>
2026-05-05 17:18:33 +0200

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CLDV6211POE.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
